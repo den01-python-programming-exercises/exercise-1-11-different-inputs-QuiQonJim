23155f950c97f084b145b7c2a43b8e6a67223dc9
jamesgildea92@protonmail.com <jamesgildea92@protonmail.com>
2021-07-02 12:38:16 +0100

    attempt 2

--- src/exercise.py
+++ src/exercise.py
@@ -4,7 +4,7 @@ def main():
     ask_float = float(input("Give a float:"))
     ask_boolean = bool(input("Give a boolean:"))#write your code below this line
 
-    print("You gave the string  " + ask_string)
+    print("You gave the string " + ask_string)
     print("You gave the integer " + str(ask_int))
     print("You gave the float " + str(ask_float))
     print("You gave the boolean " + str(ask_boolean))
