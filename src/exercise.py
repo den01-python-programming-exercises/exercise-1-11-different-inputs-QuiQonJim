776be43836f05d45ded1299417494ca2d565b07e
jamesgildea92@protonmail.com <jamesgildea92@protonmail.com>
2021-07-02 12:46:15 +0100

    attempt 3

--- src/exercise.py
+++ src/exercise.py
@@ -2,11 +2,11 @@ def main():
     ask_string = input("Give a string:")
     ask_int = int(input("Give an integer:"))
     ask_float = float(input("Give a float:"))
-    ask_boolean = bool(input("Give a boolean:"))#write your code below this line
+    ask_boolean = input("Give a boolean:")#write your code below this line
 
     print("You gave the string " + ask_string)
     print("You gave the integer " + str(ask_int))
     print("You gave the float " + str(ask_float))
-    print("You gave the boolean " + str(ask_boolean))
+    print("You gave the boolean " + ask_boolean)
 if __name__ == '__main__':
     main()
